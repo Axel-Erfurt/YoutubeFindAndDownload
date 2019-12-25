d21d2895be019f7ea26e8744a30dba12b7845f7c
Axel Schneider <axel99092@t-online.de>
2019-12-25 19:18:17 +0100

    Add files via upload

---
+++ YTFindAndDownload.py
@@ -0,0 +1,473 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Credits: searchyoutube made by LBLZR_ https://github.com/LaBlazer/searchyt - Apache License
+# ©2019 Axel Schneider https://github.com/Axel-Erfurt
+#############################################################################
+from PyQt5.QtCore import (QFile, QPoint, QRect, QSize, QStandardPaths, 
+                                            Qt, QProcess, QSettings)
+from PyQt5.QtGui import QIcon, QFont, QClipboard
+from PyQt5.QtWidgets import (QAction, QApplication, QFileDialog, QMainWindow, QLineEdit, 
+                             QProgressBar, QTableWidget, QAbstractItemView, QDockWidget, 
+            QMessageBox, QHBoxLayout, QVBoxLayout, QWidget, QLabel, QMessageBox, QPushButton, QComboBox, QTableWidgetItem)
+            
+import searchyoutube
+import YTPlayer2 as YTPlayer
+
+
+syt = searchyoutube.searchyt()
+findList = []
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        btnwidth = 160
+        self.ytdlExec = ""
+        self.ytUrl = ''
+        self.OutFolder = '/tmp'
+        self.settings = QSettings('YouTubeDLS', 'YTDL')
+        self.setAttribute(Qt.WA_DeleteOnClose)
+        self.createStatusBar()
+        pyfile = QStandardPaths.findExecutable("youtube-dl")
+        if not pyfile == "":
+            print("found " + pyfile)
+            self.ytdlExec = pyfile
+        else:
+            self.msgbox("youtube-dl not found\nPlease install youtube-dl")
+
+        self.cmd = ''
+        self.process = QProcess(self)
+        self.process.started.connect(lambda: self.showMessage("creating List"))
+        self.process.finished.connect(lambda: self.showMessage("finished creating List"))
+        self.process.finished.connect(self.processFinished)
+        self.process.readyRead.connect(self.processOut)
+
+        self.dlProcess = QProcess(self)
+        self.dlProcess.setProcessChannelMode(QProcess.MergedChannels)
+        self.dlProcess.started.connect(lambda: self.showMessage("download started"))
+        self.dlProcess.finished.connect(lambda: self.showMessage("download finished"))
+        self.dlProcess.finished.connect(lambda: self.setWindowTitle("YouTube Find & Download"))
+        self.dlProcess.readyRead.connect(self.dlProcessOut)
+
+        self.list = []
+        
+        self.setContentsMargins(0,0,15,5)
+
+        self.move(0, 0)
+        self.setFixedSize(1080, 660)
+        self.setStyleSheet(myStyleSheet(self))
+        self.setWindowIcon(QIcon.fromTheme("youtube"))
+        #### path
+        lblFind = QLabel()
+        lblFind.setText("find:")
+        lblFind.setAlignment(Qt.AlignRight)
+        lblFind.setFixedWidth(btnwidth)
+        lblFind.setFont(QFont("Noto Sans", 9))
+        lblFind.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
+        self.findField = QLineEdit("")
+        self.findField.addAction(QIcon.fromTheme("edit-find"), QLineEdit.LeadingPosition)
+        self.findField.setPlaceholderText("insert search term and press ENTER to get list of available movies")
+        self.findField.returnPressed.connect(self.findItems)
+
+        vlayout = QVBoxLayout()
+        hlayout = QHBoxLayout()
+        vlayout.addWidget(self.findField)
+        
+        self.lb = QTableWidget()
+        self.lb.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.lb.setDragDropMode(QAbstractItemView.DragOnly)
+        self.lb.setToolTip("on selection change the Youtube ID is in clipboard\ndouble click to create list of available resolutions for  download")
+        self.lb.setColumnCount(2)
+        self.lb.setColumnWidth(0, 540)
+        self.lb.hideColumn(1)
+        #self.lb.setColumnWidth(1, 0)
+        self.lb.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.lb.doubleClicked.connect(self.getItem)
+        self.lb.itemSelectionChanged.connect(self.copyURL)
+        self.lb.setHorizontalHeaderLabels(["Title", "ID"])
+        self.lb.setFixedHeight(380)
+        vlayout.addLayout(hlayout)
+        vlayout.addWidget(self.lb)
+
+        #### output path
+        btnOutPath = QPushButton()
+        btnOutPath.setFont(QFont("Noto Sans", 9))
+        btnOutPath.setIcon(QIcon.fromTheme("gtk-open"))
+        btnOutPath.setText("select Output Folder")
+        btnOutPath.setFixedWidth(btnwidth)
+        btnOutPath.clicked.connect(self.openOutFolder)
+        self.lblOutPath = QLineEdit()
+        self.lblOutPath.setPlaceholderText("insert output folder path")
+        self.lblOutPath.textChanged.connect(self.updateOutputPath)
+
+        hlayout2 = QHBoxLayout()
+        hlayout2.addWidget(btnOutPath)
+        hlayout2.addWidget(self.lblOutPath)
+
+        #### ytdlExec path
+        btnYTDLpath = QPushButton()
+        btnYTDLpath.setFont(QFont("Noto Sans", 9))
+        btnYTDLpath.setIcon(QIcon.fromTheme("document-open"))
+        btnYTDLpath.setText("select youtube-dl")
+        btnYTDLpath.setFixedWidth(btnwidth)
+        btnYTDLpath.clicked.connect(self.selectYTDL)
+        self.lblYTDLpath = QLineEdit(str(self.ytdlExec))
+        self.lblYTDLpath.textChanged.connect(self.updatelblYTDLpath)
+        self.lblYTDLpath.setPlaceholderText("insert path to youtube-dl")
+
+
+        hlayout3 = QHBoxLayout()
+        hlayout3.addWidget(btnYTDLpath)
+        hlayout3.addWidget(self.lblYTDLpath)
+
+        dlBtn = QPushButton()
+        dlBtn.setIcon(QIcon.fromTheme("download"))
+        dlBtn.setText("Download")
+        dlBtn.clicked.connect(self.downloadSelected)
+        dlBtn.setFixedWidth(100)
+
+        dlCancelBtn = QPushButton()
+        dlCancelBtn.setIcon(QIcon.fromTheme("cancel"))
+        dlCancelBtn.setText("Cancel")
+        dlCancelBtn.clicked.connect(self.cancelDownload)
+        dlCancelBtn.setFixedWidth(100)
+        
+        empty = QWidget()
+        
+        playBtn = QPushButton()
+        playBtn.setIcon(QIcon.fromTheme("media-playback-start"))
+        playBtn.setText("Play")
+        playBtn.clicked.connect(self.playMovie)
+        playBtn.setFixedWidth(100)
+
+        self.dlCombo = QComboBox()
+        self.dlCombo.setFixedHeight(26)
+
+        self.pbar = QProgressBar()
+        self.pbar.setFixedHeight(16)
+        self.pbar.setFont(QFont("Helvetica", 7))
+        self.pbar.setMaximum(100)
+        self.pbar.setMinimum(0)
+        self.pbar.setValue(0)
+
+        btnLayout = QHBoxLayout()
+        btnLayout.addWidget(dlBtn)
+        btnLayout.addWidget(dlCancelBtn)
+        btnLayout.addWidget(empty)
+        btnLayout.addWidget(playBtn)
+
+        vlayout.addLayout(hlayout2)
+        vlayout.addLayout(hlayout3)
+        vlayout.addWidget(self.dlCombo)
+        vlayout.addWidget(self.pbar)
+        vlayout.addLayout(btnLayout)
+
+        mywidget = QWidget()
+        mywidget.setLayout(vlayout)
+        
+        self.hbox = QHBoxLayout()
+        self.hbox.addWidget(mywidget)
+        
+        self.player = YTPlayer.VideoPlayer('')
+        self.playerbox = QVBoxLayout()
+        self.playerbox.addWidget(self.player)
+        
+        self.dwid = QDockWidget("Player")
+        self.dwid.topLevelChanged.connect(self.makeFloating)
+        self.dwid.setFeatures(QDockWidget.DockWidgetFloatable)
+        self.dwid.setFixedSize(480, 320)
+        self.pwid = QWidget()
+        self.pwid.setLayout(self.playerbox)
+        self.dwid.setWidget(self.pwid)
+        
+        self.cwid = QWidget()
+        self.cwid.setLayout(self.hbox)
+
+        self.setCentralWidget(self.cwid)
+        self.addDockWidget(Qt.RightDockWidgetArea, self.dwid)
+
+        self.clip = QApplication.clipboard()
+
+        self.readSettings()
+        self.setWindowTitle("YouTube Find & Download")
+        
+    def makeFloating(self):
+        if self.dwid.isFloating() == False:
+            self.dwid.setFixedSize(480, 320)
+        else:
+            g = QApplication.desktop().screenGeometry()
+            self.dwid.setFixedSize(g.width(), g.height() - 30)
+            self.dwid.move(0, 0)
+        
+    def mouseDoubleClickEvent(self, event):
+        if self.player.underMouse():
+            if event.button() == Qt.LeftButton:
+                if self.dwid.isFloating() == False:
+                    self.dwid.setFloating(True)
+                    g = QApplication.desktop().screenGeometry()
+                    self.dwid.setFixedSize(g.width(), g.height() - 30)
+                    self.dwid.move(0, 0)
+
+        
+    def playMovie(self):
+        row = self.selectedRow()
+        item = self.lb.item(row, 1 ).text()
+        if not item == None:
+            if self.player:
+                self.player.myurl = item
+                self.player.getYTUrl()
+                #self.fillCombo(item)
+            else:
+                print("player already visible")
+            
+        
+    def dragMoveEvent(self, event):
+        if event.source() == self.lb:
+            if event.buttons() & Qt.LeftButton:
+                row = self.selectedRow()
+                item = self.lb.item(row, 1 ).text()
+                if not item == None:
+                    name = item
+                    print(name)
+                    print(event.mimeData().text())
+                event.accept()
+        else:
+            print("not lb")
+        
+    def findItems(self):
+        self.lb.clearContents()
+        self.lb.setRowCount(0)
+        searchText = self.findField.text()
+        res = syt.search(searchText)
+        self.statusBar().showMessage("%s %s %s" % ("found:", len(res), "matches"), 0)
+        if len(res):
+            for x in range(len(res)):
+                #print(r)
+                title = res[x].get("title")
+                url = res[x].get("id")
+                #print("%s:\n%s\n%s" % (x + 1, title, url))
+                findList.append(url)
+                self.lb.insertRow(x)
+                t = QTableWidgetItem(title)
+                u = QTableWidgetItem(url)
+                self.lb.setItem(x, 0, t)
+                self.lb.setItem(x, 1, u)
+            for x in range(self.lb.rowCount()):
+                self.lb.resizeRowToContents(x)
+        else:
+            self.statusBar().showMessage("nothing found", 0)
+
+    def closeEvent(self, e):
+        self.writeSettings()
+        e.accept()
+
+    def readSettings(self):
+        print("reading settings")
+        if self.settings.contains('geometry'):
+            self.setGeometry(self.settings.value('geometry'))
+        if self.settings.contains('outFolder'):
+            self.lblOutPath.setText(self.settings.value('outFolder'))
+
+    def writeSettings(self):
+        print("writing settings")
+        self.settings.setValue('outFolder', self.OutFolder)
+        self.settings.setValue('geometry', self.geometry())
+
+    def updateOutputPath(self):
+        self.OutFolder = self.lblOutPath.text()
+        self.showMessage("output path changed to: " + self.lblOutPath.text())
+
+    def updatelblYTDLpath(self):
+        self.ytdlExec = self.lblYTDLpath.text()
+        self.showMessage("youtube-dl path changed to: " +self.lblYTDLpath.text())
+
+    def showMessage(self, message):
+        self.statusBar().showMessage(message, 0)
+
+    def selectYTDL(self):
+         fileName,_ = QFileDialog.getOpenFileName(self, "locate ytdlExec", "/usr/local/bin/ytdlExec",  "exec Files (*)")
+         if fileName:
+            self.lblYTDLpath.setText(fileName)
+            self.ytdlExec = fileName
+
+    def openOutFolder(self):
+        dlg = QFileDialog()
+        dlg.setFileMode(QFileDialog.Directory)
+        fileName = dlg.getExistingDirectory()
+        if fileName:
+            self.lblOutPath.setText(fileName)
+            
+    def getItem(self):
+        row = self.selectedRow()
+        item = self.lb.item(row, 1 ).text()
+        #column = 1
+        if not item == None:
+            name = item
+            self.fillCombo(name)
+        else:
+            name = ""
+
+    def copyURL(self):
+        row = self.selectedRow()
+        item = self.lb.item(row, 1 ).text()
+        self.clip.setText(item)
+
+    def selectedRow(self):
+        if self.lb.selectionModel().hasSelection():
+            row =  self.lb.selectionModel().selectedIndexes()[0].row()
+            return int(row)
+
+    def selectedColumn(self):
+        column =  self.lb.selectionModel().selectedIndexes()[0].column()
+        return int(column)
+
+
+    def fillCombo(self, name):
+        self.dlCombo.clear()
+        if QFile.exists(self.ytdlExec):
+            self.list = []
+            self.ytUrl = name
+            if not name == "":
+                print("fill Combo")
+                self.process.start(self.ytdlExec,['-F', self.ytUrl])
+        else:
+            self.showMessage("youtube-dl missing")
+        
+    def processOut(self):
+            try:
+                output = str(self.process.readAll(), encoding = 'utf8').rstrip()
+            except Error:
+                output = str(self.process.readAll()).rstrip()          
+            self.list.append(output)
+
+    def processFinished(self):
+            out = ','.join(self.list)
+            out = out.partition("resolution note")[2]
+            out = out.partition('\n')[2]
+            mylist = out.rsplit('\n')
+            self.dlCombo.addItems(mylist)
+            count = self.dlCombo.count()
+            self.dlCombo.setCurrentIndex(count-1)
+
+    def downloadSelected(self):
+        if QFile.exists(self.ytdlExec):
+            self.pbar.setValue(0)
+            quality = self.dlCombo.currentText().partition(" ")[0]
+            options = []
+            options.append('-f')
+            options.append(quality)
+            options.append('--add-metadata')
+            options.append("-o")
+            options.append("%(title)s.%(ext)s")
+            options.append(self.ytUrl)
+            if not quality == "":
+                self.showMessage("download started")
+                print("download selected quality:", quality)
+                self.dlProcess.setWorkingDirectory(self.OutFolder)
+                self.dlProcess.start(self.ytdlExec, options)
+            else:
+                self.showMessage("list of available files is empty")
+        else:
+           self.showMessage("youtube-dl missing")
+
+    def dlProcessOut(self):
+        try:
+            out = str(self.dlProcess.readAll(), encoding = 'utf8').rstrip()
+        except Error:
+            out = str(self.dlProcess.readAll()).rstrip()
+        out = out.rpartition("[download] ")[2]
+        self.showMessage("Progress: " + out)
+        self.setWindowTitle(out)
+        out = out.rpartition("%")[0].rpartition(".")[0]
+        if not out == "":
+            try:
+               pout = int(out)
+               self.pbar.setValue(pout)
+            except ValueError:
+                pass
+
+    def cancelDownload(self):
+        if self.dlProcess.state() == QProcess.Running:
+            print("process is running, will be cancelled")
+            self.dlProcess.close()
+            self.showMessage("Download cancelled")
+            self.pbar.setValue(0)
+        else:
+            self.showMessage("process is not running")
+
+    def createStatusBar(self):
+        self.statusBar().showMessage("Ready")
+
+    def msgbox(self, message):
+        QMessageBox.warning(self, "Message", message)
+
+def myStyleSheet(self):
+    return """
+
+QStatusBar
+{
+font-family: Helvetica;
+font-size: 8pt;
+color: #666666;
+}
+QMenuBar
+{
+background: transparent;
+border: 0px;
+}
+QToolBar
+{
+background: transparent;
+border: 0px;
+}
+QMainWindow
+{
+     background: qlineargradient(y1: 0, y2: 1,
+                                 stop: 0 #E1E1E1, stop: 0.4 #DDDDDD,
+                                 stop: 0.5 #D8D8D8, stop: 1.0 #D3D3D3);
+}
+QTableWidget
+{
+     background: qlineargradient(y1: 0, y2: 1,
+                                 stop: 0 #E1E1E1, stop: 0.4 #DDDDDD,
+                                 stop: 0.5 #D8D8D8, stop: 1.0 #D3D3D3);
+}
+QHeaderView::section
+{
+background-color:#d3d7cf;
+color: #204a87; 
+font: bold
+}
+QHeaderView
+{
+     background: qlineargradient(y1: 0, y2: 1,
+                                 stop: 0 #E1E1E1, stop: 0.4 #DDDDDD,
+                                 stop: 0.5 #D8D8D8, stop: 1.0 #D3D3D3);
+}
+QTableCornerButton::section 
+{
+background-color:#d3d7cf; 
+}
+QLineEdit
+{
+     background: qlineargradient(y1: 0, y2: 1,
+                                 stop: 0 #E1E1E1, stop: 0.4 #e5e5e5,
+                                 stop: 0.5 #e9e9e9, stop: 1.0 #d2d2d2);
+}
+QPushButton
+{
+background: #babdb6;
+}
+QComboBox
+{
+background: #babdb6;
+}
+    """    
+
+
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    mainWin = MainWindow()
+    mainWin.show()
+    sys.exit(app.exec_())
---
+++ YTPlayer2.py
@@ -0,0 +1,189 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtGui import QPalette, QKeySequence, QIcon
+from PyQt5.QtCore import QDir, Qt, QUrl, QSize, QPoint, QTime, QMimeData, QProcess, QEvent
+from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer, QMediaMetaData
+from PyQt5.QtMultimediaWidgets import QVideoWidget
+from PyQt5.QtWidgets import (QApplication, QFileDialog, QHBoxLayout, QLineEdit,
+                            QPushButton, QSizePolicy, QSlider, QMessageBox, QStyle, QVBoxLayout,  
+                            QWidget, QShortcut, QMenu)
+import sys
+import os
+import subprocess
+#QT_DEBUG_PLUGINS
+
+class VideoPlayer(QWidget):
+
+    def __init__(self, aPath, parent=None):
+        super(VideoPlayer, self).__init__(parent)
+
+        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.StreamPlayback)
+        self.mediaPlayer.mediaStatusChanged.connect(self.printMediaData)
+        self.mediaPlayer.setVolume(80)
+        self.videoWidget = QVideoWidget(self)
+        
+        self.lbl = QLineEdit('00:00:00')
+        self.lbl.setReadOnly(True)
+        self.lbl.setFixedWidth(70)
+        self.lbl.setUpdatesEnabled(True)
+        self.lbl.setStyleSheet(stylesheet(self))
+        
+        self.elbl = QLineEdit('00:00:00')
+        self.elbl.setReadOnly(True)
+        self.elbl.setFixedWidth(70)
+        self.elbl.setUpdatesEnabled(True)
+        self.elbl.setStyleSheet(stylesheet(self))
+
+        self.playButton = QPushButton()
+        self.playButton.setEnabled(False)
+        self.playButton.setFixedWidth(32)
+        #self.playButton.setStyleSheet("background: black")
+        self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+        self.playButton.clicked.connect(self.play)
+
+        self.positionSlider = QSlider(Qt.Horizontal, self)
+        #self.positionSlider.setStyleSheet (stylesheet(self)) 
+        self.positionSlider.setRange(0, 100)
+        self.positionSlider.sliderMoved.connect(self.setPosition)
+        self.positionSlider.sliderMoved.connect(self.handleLabel)
+        self.positionSlider.setSingleStep(2)
+        self.positionSlider.setPageStep(20)
+        self.positionSlider.setAttribute(Qt.WA_TranslucentBackground, True)
+
+        self.process = QProcess(self)
+        self.process.readyRead.connect(self.dataReady)
+        self.process.finished.connect(self.playFromURL)
+
+        self.myurl = ""
+        
+        controlLayout = QHBoxLayout()
+        controlLayout.setContentsMargins(5, 0, 5, 0)
+        controlLayout.addWidget(self.playButton)
+        controlLayout.addWidget(self.lbl)
+        controlLayout.addWidget(self.positionSlider)
+        controlLayout.addWidget(self.elbl)
+
+        layout = QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(self.videoWidget)
+        layout.addLayout(controlLayout)
+
+        self.setLayout(layout)
+        #self.setFixedSize(480, 300)
+
+        self.mediaPlayer.setVideoOutput(self.videoWidget)
+        self.mediaPlayer.stateChanged.connect(self.mediaStateChanged)
+        self.mediaPlayer.positionChanged.connect(self.positionChanged)
+        self.mediaPlayer.positionChanged.connect(self.handleLabel)
+        self.mediaPlayer.durationChanged.connect(self.durationChanged)
+        self.mediaPlayer.error.connect(self.handleError)
+
+        self.suspend_screensaver()
+
+    def playFromURL(self):
+        self.mediaPlayer.pause()
+        self.mediaPlayer.setMedia(QMediaContent(QUrl(self.myurl)))
+        self.playButton.setEnabled(True)
+        self.mediaPlayer.play()
+        print(self.myurl)
+
+    def getYTUrl(self):
+        cmd = "youtube-dl -g -f best " + self.myurl
+        print("grabbing YouTube URL")
+        self.process.start(cmd)
+
+    def dataReady(self):
+        self.myurl = str(self.process.readAll(), encoding = 'utf8').rstrip() ###
+        self.myurl = self.myurl.partition("\n")[0]
+        print(self.myurl)
+        self.playFromURL()
+
+    def suspend_screensaver(self):
+        'suspend linux screensaver'
+        proc = subprocess.Popen('gsettings set org.gnome.desktop.screensaver idle-activation-enabled false', shell=True)
+        proc.wait()
+
+    def resume_screensaver(self):
+        'resume linux screensaver'
+        proc = subprocess.Popen('gsettings set org.gnome.desktop.screensaver idle-activation-enabled true', shell=True)
+        proc.wait()
+
+    def play(self):
+        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+            self.mediaPlayer.pause()
+        else:
+            self.mediaPlayer.play()
+    
+    def mediaStateChanged(self, state):
+        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+            self.playButton.setIcon(
+                    self.style().standardIcon(QStyle.SP_MediaPause))
+        else:
+            self.playButton.setIcon(
+                    self.style().standardIcon(QStyle.SP_MediaPlay))
+
+    def positionChanged(self, position):
+        self.positionSlider.setValue(position)
+        
+    def durationChanged(self, duration):
+        self.positionSlider.setRange(0, duration)
+        mtime = QTime(0,0,0,0)
+        mtime = mtime.addMSecs(self.mediaPlayer.duration())
+        self.elbl.setText(mtime.toString())
+
+    def setPosition(self, position):
+        self.mediaPlayer.setPosition(position)
+
+    def handleError(self):
+        self.playButton.setEnabled(False)
+        print("Error: ", self.mediaPlayer.errorString())
+    
+    def forwardSlider(self):
+        self.mediaPlayer.setPosition(self.mediaPlayer.position() + 1000*60)
+
+    def forwardSlider10(self):
+            self.mediaPlayer.setPosition(self.mediaPlayer.position() + 10000*60)
+
+    def backSlider(self):
+        self.mediaPlayer.setPosition(self.mediaPlayer.position() - 1000*60)
+
+    def backSlider10(self):
+        self.mediaPlayer.setPosition(self.mediaPlayer.position() - 10000*60)
+        
+    def volumeUp(self):
+        self.mediaPlayer.setVolume(self.mediaPlayer.volume() + 10)
+        print("Volume: " + str(self.mediaPlayer.volume()))
+    
+    def volumeDown(self):
+        self.mediaPlayer.setVolume(self.mediaPlayer.volume() - 10)
+        print("Volume: " + str(self.mediaPlayer.volume()))
+        
+
+    def printMediaData(self):
+        if self.mediaPlayer.mediaStatus() == 6:
+            if self.mediaPlayer.isMetaDataAvailable():
+                res = str(self.mediaPlayer.metaData("Resolution")).partition("PyQt5.QtCore.QSize(")[2].replace(", ", "x").replace(")", "")
+                print("%s%s" % ("Video Resolution = ",res))
+            else:
+                print("no metaData available")
+
+##################### update Label ##################################
+    def handleLabel(self):
+            self.lbl.clear()
+            mtime = QTime(0,0,0,0)
+            self.time = mtime.addMSecs(self.mediaPlayer.position())
+            self.lbl.setText(self.time.toString())
+###################################################################
+
+def stylesheet(self):
+    return """
+QLineEdit
+{
+background: transparent;
+color: #2e3436;
+border: 0px;
+font-size: 8pt;
+font-weight: bold;
+}
+    """
